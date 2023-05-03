952513d417d4355480758ec071d2916b8536f6e1
Dima Denysenko <coding@dimdnk.com>
2023-05-03 11:05:26 +0300

    chore: upgrade dependency versions & pipeline commands

--- src/main/java/org/ujar/jh/petclinic/vuewebflux/web/rest/AccountResource.java
+++ src/main/java/org/ujar/jh/petclinic/vuewebflux/web/rest/AccountResource.java
@@ -156,7 +156,7 @@ public class AccountResource {
     }
 
     /**
-     * {@code POST   /account/reset-password/init} : Send an email to reset the password of the user.
+     * {@code POST   /account/reset-password/init} : Send email to reset the password of the user.
      *
      * @param mail the mail of the user.
      */
